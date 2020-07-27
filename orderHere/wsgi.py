4830be24ae9230ca4febf9fecc391079cc8671a2
Houghelpuff <jacobhough2017@gmail.com>
2020-07-27 14:20:39 -0600

    messing with wsgi and settings files

--- orderHere/wsgi.py
+++ orderHere/wsgi.py
@@ -12,6 +12,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orderHere.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orderHere.settings.dev')
 
 application = get_wsgi_application()
