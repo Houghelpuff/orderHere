043ebf34d5484b55f687f95d6f3ff6ab46a56c39
Houghelpuff <jacobhough2017@gmail.com>
2020-07-27 14:56:59 -0600

    grasping at nothing lol

--- orderHere/settings.py
+++ orderHere/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False, logging=False)
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES.update(prod_db)
